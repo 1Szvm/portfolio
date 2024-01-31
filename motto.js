6b7699986996dd4f690f584f341a6e26da2f69dc
1Szvm <szilajmark2005@gmail.com>
2024-01-31 13:51:55 +0100

    Update

--- motto.js
+++ motto.js
@@ -1,4 +1,11 @@
-import { motto,panelBackgrounds } from "./assets.js";
+const motto="Try Fail Learn Succeed Repeat".split(" ")
+const panelBackgrounds = [
+    'https://source.unsplash.com/NtbsSBlnMm0/1500x1500&#39',
+    'https://source.unsplash.com/aig06b_ujUk/1500x1500&#39',
+    'https://source.unsplash.com/s9CC2SKySJM/1500x1500&#39',
+    'https://source.unsplash.com/aVJh5bgtd8o/1500x1500&#39',
+    'https://source.unsplash.com/3MNzGlQM7qs/1500x1500&#39'
+];
 
 motto.forEach(word=>{
     document.querySelector('.panels').innerHTML+=`<div class="panel">${word}</div>`
